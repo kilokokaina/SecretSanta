fd31e4a199ff8a2886e572d83f25b91945db1bce
kilokokaina <nikolaushki@gmail.com>
2025-12-03 01:43:43 +0800

    Temp

--- src/main/java/org/work/secretsantabot/SecretSantaBot.java
+++ src/main/java/org/work/secretsantabot/SecretSantaBot.java
@@ -8,15 +8,9 @@ import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsume
 import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
-
-import static java.lang.Math.toIntExact;
 
 @Component
 public class SecretSantaBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
@@ -38,8 +32,6 @@ public class SecretSantaBot implements SpringLongPollingBot, LongPollingSingleTh
         return this;
     }
 
-
-
     @Override
     public void consume(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
@@ -49,61 +41,15 @@ public class SecretSantaBot implements SpringLongPollingBot, LongPollingSingleTh
 
             log.info("Username: {}, text: {}", userName, messageText);
 
-            switch (messageText) {
-                case "/manage_session" -> {
-                    var message = SendMessage
-                            .builder()
-                            .chatId(chatId)
-                            .text(messageText)
-                            .replyMarkup(
-                                    InlineKeyboardMarkup.builder().keyboardRow(
-                                            new InlineKeyboardRow(InlineKeyboardButton
-                                                    .builder()
-                                                    .text("Закрыть регистрацию")
-                                                    .callbackData("update_msg_text")
-                                                    .build()
-                                            )
-                                    )
-                                    .build()
-                            )
-                            .build();
-                    try {
-                        telegramClient.execute(message);
-                    } catch (TelegramApiException e) {
-                        log.error(e.getMessage());
-                    }
-                }
-                default -> {
-                    var message = SendMessage
-                            .builder()
-                            .chatId(chatId)
-                            .text(messageText)
-                            .build();
-                    try {
-                        telegramClient.execute(message);
-                    } catch (TelegramApiException e) {
-                        log.error(e.getMessage());
-                    }
-                }
-            }
-        } else if (update.hasCallbackQuery()) {
-            // Set variables
-            String call_data = update.getCallbackQuery().getData();
-            long message_id = update.getCallbackQuery().getMessage().getMessageId();
-            long chat_id = update.getCallbackQuery().getMessage().getChatId();
-
-            if (call_data.equals("update_msg_text")) {
-                String answer = "Updated message text";
-                EditMessageText new_message = EditMessageText.builder()
-                        .chatId(chat_id)
-                        .messageId(toIntExact(message_id))
-                        .text(answer)
-                        .build();
-                try {
-                    telegramClient.execute(new_message);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
+            var message = SendMessage
+                    .builder()
+                    .chatId(chatId)
+                    .text(messageText)
+                    .build();
+            try {
+                telegramClient.execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e.getMessage());
             }
         }
     }
