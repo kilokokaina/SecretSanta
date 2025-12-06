309d911520bb389192337a6a42e1c1024dd4b8f2
kilokokaina <nikolaushki@gmail.com>
2025-12-06 11:33:13 +0800

    Temp

--- src/main/java/org/work/secretsantabot/controller/MainController.java
+++ src/main/java/org/work/secretsantabot/controller/MainController.java
@@ -1,14 +1,30 @@
 package org.work.secretsantabot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.work.secretsantabot.service.SessionService;
 
 @Controller
 public class MainController {
 
+    private final SessionService sessionService;
+
+    @Autowired
+    public MainController(SessionService sessionService) {
+        this.sessionService = sessionService;
+    }
+
     @GetMapping
     public String hello() {
         return "index";
+    }
+
+    @GetMapping("session/{sessionId}")
+    public String session(@PathVariable String sessionId, Model model) {
+        return "session";
     }
 
 }
