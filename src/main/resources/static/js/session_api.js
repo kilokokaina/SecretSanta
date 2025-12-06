2669fac3661a9c101d53cd15c8ed3b2e0d04b8c7
kilokokaina <nikolaushki@gmail.com>
2025-12-06 20:29:29 +0800

    Temp

--- src/main/resources/static/js/session_api.js
+++ src/main/resources/static/js/session_api.js
@@ -46,7 +46,7 @@ function getSessions() {
         let participantListHTML = document.querySelector('.participant-session-list').innerHTML;
         if (participantList.length > 0) participantListHTML = '';
         participantList.forEach(participantItem => {
-            let sessionStatus = (participantItem.status) ? '🔓' : '🔒';
+            let sessionStatus = (participantItem.status) ? '<img th:src="@{/img/open.png}" width="28" height="28">' : '<img th:src="@{/img/close.png}" width="28" height="28">';
             participantListHTML += `
                 <div class="session-item">
                     <div class="title">${participantItem.sessionName}</div>
